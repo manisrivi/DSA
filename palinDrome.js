87aaa667080c9e3edef405313dc41feec6e41dd6
Manikandan S <manisrivi1995@gmail.com>
2025-06-24 23:21:08 +0530

    palindrome

---
+++ palinDrome.js
@@ -0,0 +1,23 @@
+const palinDrome = (x) => {
+
+    if (x < 0) return false;
+
+    let xCopy = x;
+    let rev = 0;
+    
+    while (x > 0) {
+        let rem = x % 10;
+        rev = (10 * rev) + rem;
+        x = Math.floor(x / 10);
+    }
+
+    if (rev == xCopy) {
+        return true
+    } else {
+        return false
+    }
+
+}
+
+
+console.log(palinDrome(121))
